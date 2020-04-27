6d4f615bb8595c8ae80f0016d05e95a1553b67be
Даниил Александрович Аникин <287824@niuitmo.ru>
2020-04-27 23:52:27 +0300

    Добавлены тесты

---
+++ VigenereCipher.WPF/VigenereCipher.WPF.Test/MainWindowViewModelTest.cs
@@ -0,0 +1,56 @@
+﻿using CourseworkTest.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VigenereCipher.WPF.ViewModels;
+
+namespace CourseworkTest
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        private MainWindowViewModel viewModel;
+        
+        [TestMethod]
+        public void SetDecryptModeCommandTest()
+        {
+            viewModel = new MainWindowViewModel(new MockDialogService());
+            // По умолчанию значение Mode = true
+            Assert.IsTrue(viewModel.Mode);
+
+            viewModel.SetDecryptModeCommand.Execute(new object());
+            // Значение должно быть Mode = false
+            Assert.IsFalse(viewModel.Mode);
+        }
+
+        [TestMethod]
+        public void SetEncryptModeCommandTest()
+        {
+            viewModel = new MainWindowViewModel(new MockDialogService());
+            // По умолчанию значение Mode = true
+            Assert.IsTrue(viewModel.Mode);
+
+            viewModel.SetEncryptModeCommand.Execute(new object());
+            // Значение должно быть Mode = true
+            Assert.IsTrue(viewModel.Mode);
+        }
+
+        [TestMethod]
+        public void ActionCommandTest()
+        {
+            viewModel = new MainWindowViewModel(new MockDialogService());
+            // По умолчанию значение должно быть false
+            Assert.IsFalse(viewModel.ActionCommand.CanExecute(new object()));
+
+            // Значение должно быть false
+            viewModel.Key = "ключ";
+            Assert.IsFalse(viewModel.ActionCommand.CanExecute(new object()));
+
+            // Значение должно быть true
+            viewModel.Text = "текст";
+            Assert.IsTrue(viewModel.ActionCommand.CanExecute(new object()));
+
+            // Значение должно быть false
+            viewModel.Key = null;
+            Assert.IsFalse(viewModel.ActionCommand.CanExecute(new object()));
+        }
+    }
+}
---
+++ VigenereCipher.WPF/VigenereCipher.WPF.Test/Mocks/MockDialogService.cs
@@ -0,0 +1,87 @@
+﻿using MvvmDialogs;
+using MvvmDialogs.FrameworkDialogs.FolderBrowser;
+using MvvmDialogs.FrameworkDialogs.MessageBox;
+using MvvmDialogs.FrameworkDialogs.OpenFile;
+using MvvmDialogs.FrameworkDialogs.SaveFile;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseworkTest.Mocks
+{
+    class MockDialogService : IDialogService
+    {
+        public bool Activate(INotifyPropertyChanged viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show<[NullableAttribute(0)] T>(INotifyPropertyChanged ownerViewModel, INotifyPropertyChanged viewModel) where T : System.Windows.Window
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show(INotifyPropertyChanged ownerViewModel, INotifyPropertyChanged viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowCustom<[NullableAttribute(0)] T>(INotifyPropertyChanged ownerViewModel, INotifyPropertyChanged viewModel) where T : IWindow
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowCustomDialog<[NullableAttribute(0)] T>(INotifyPropertyChanged ownerViewModel, IModalDialogViewModel viewModel) where T : IWindow
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowDialog<[NullableAttribute(0)] T>(INotifyPropertyChanged ownerViewModel, IModalDialogViewModel viewModel) where T : System.Windows.Window
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowDialog(INotifyPropertyChanged ownerViewModel, IModalDialogViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowFolderBrowserDialog(INotifyPropertyChanged ownerViewModel, FolderBrowserDialogSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Windows.MessageBoxResult ShowMessageBox(INotifyPropertyChanged ownerViewModel, string messageBoxText, string caption = "", System.Windows.MessageBoxButton button = 0, System.Windows.MessageBoxImage icon = 0, System.Windows.MessageBoxResult defaultResult = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Windows.MessageBoxResult ShowMessageBox(INotifyPropertyChanged ownerViewModel, MessageBoxSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowOpenFileDialog(INotifyPropertyChanged ownerViewModel, OpenFileDialogSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ShowSaveFileDialog(INotifyPropertyChanged ownerViewModel, SaveFileDialogSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+// Заглушка
+namespace CourseworkTest
+{
+    class NullableAttribute : Attribute
+    {
+        public NullableAttribute(int v)
+        {
+        }
+    }
+}
